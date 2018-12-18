3424ac646a20230a05fff43dd6e2146e606a90f4
Norman Hooper <norman@kaapstorm.com>
2018-12-19 00:34:09 +0200

    Update settings

--- src/clubbable/clubbable/settings.py
+++ src/clubbable/clubbable/settings.py
@@ -69,18 +69,21 @@ INSTALLED_APPS = (
     'docs',
     'dropboxer',
     'galleries',
+    'import_legacy',
+    'import_mdb',
     'mailer',
     'website',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+]
 
 ROOT_URLCONF = 'clubbable.urls'
 
