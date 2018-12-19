c80b8ab568d3ab4aae6db0887143b9204ce63613
Norman Hooper <norman@kaapstorm.com>
2018-12-20 00:47:47 +0200

    Move settings and dev DB

--- src/clubbable/clubbable/settings/example.py
+++ src/clubbable/clubbable/settings/example.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 # ==============================
 # Settings specific to clubbable
@@ -111,7 +111,7 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'clubbable', 'db', 'db.sqlite3'),
     },
     'legacy': {
         'ENGINE': 'django.db.backends.mysql',
