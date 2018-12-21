88b12596841471d63ec4794be4084c8b97ce5ae8
Norman Hooper <norman@kaapstorm.com>
2018-12-21 05:07:31 +0200

    Add account and lock to DropboxUser

--- src/clubbable/dropboxer/models.py
+++ src/clubbable/dropboxer/models.py
@@ -4,6 +4,8 @@ from club.models import GetOrNoneManager, User
 
 class DropboxUser(models.Model):
     user = models.OneToOneField(User, models.CASCADE)
+    account = models.CharField(max_length=255, blank=True, db_index=True)
     access_token = models.CharField(max_length=255, blank=True)
+    is_locked = models.BooleanField(default=False)
 
     objects = GetOrNoneManager()
