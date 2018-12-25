2b658a87248915a416538aed92f81d9de320af2f
Norman Hooper <norman@kaapstorm.com>
2018-12-25 10:43:17 +0200

    Drop redundant null

--- src/clubbable/galleries/models.py
+++ src/clubbable/galleries/models.py
@@ -35,8 +35,8 @@ class Image(models.Model):
         Meeting, models.SET_NULL,
         null=True, blank=True,
     )
-    members = models.ManyToManyField(Member, null=True, blank=True)
-    guests = models.ManyToManyField(Guest, null=True, blank=True)
+    members = models.ManyToManyField(Member, blank=True)
+    guests = models.ManyToManyField(Guest, blank=True)
     creator = models.ForeignKey(
         Member, models.SET_NULL,
         related_name='creator_image', null=True, blank=True,
