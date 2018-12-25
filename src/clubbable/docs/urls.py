3f751503906ca0f61148dcef109d621fcb97bf39
Norman Hooper <norman@kaapstorm.com>
2018-12-25 12:10:09 +0200

    Fix doc URLs

--- src/clubbable/docs/urls.py
+++ src/clubbable/docs/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from docs.views import *
 
 urlpatterns = [
-    url(r'^(\d+)/$', DocList.as_view(), name='doc_list'),
-    url(r'^(\d+)/(?P<pk>\d+)/([^/]+)/$', download, name='doc_download'),
-    url(r'^(\d+)/(?P<pk>\d+)/$', send, name='doc_send'),
+    url(r'^(?P<folder_id>\d+)/$', DocList.as_view(), name='doc_list'),
+    url(r'^(?P<folder_id>\d+)/(?P<pk>\d+)/(?P<filename>[^/]+)$', download,
+        name='doc_download'),
+    url(r'^(?P<folder_id>\d+)/(?P<pk>\d+)/send/$', send, name='doc_send'),
 ]
