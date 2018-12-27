636177fb6396e86398aef81b1f915eccaa40f41d
Norman Hooper <norman@kaapstorm.com>
2018-12-27 03:46:25 +0200

    Save cursor

--- src/clubbable/dropboxer/models.py
+++ src/clubbable/dropboxer/models.py
@@ -6,6 +6,7 @@ class DropboxUser(models.Model):
     user = models.OneToOneField(User, models.CASCADE)
     account = models.CharField(max_length=255, blank=True, db_index=True)
     access_token = models.CharField(max_length=255, blank=True)
+    cursor = models.CharField(max_length=255, blank=True)
     is_locked = models.BooleanField(default=False)
 
     objects = GetOrNoneManager()
