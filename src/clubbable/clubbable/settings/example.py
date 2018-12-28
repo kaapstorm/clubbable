7c460f27d5d119c457fefd26779974a667b8425d
Norman Hooper <norman@kaapstorm.com>
2018-12-28 02:59:10 +0200

    Support shitty hashers

--- src/clubbable/clubbable/settings/example.py
+++ src/clubbable/clubbable/settings/example.py
@@ -98,6 +98,15 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+    'django.contrib.auth.hashers.UnsaltedMD5PasswordHasher',
+]
+
 ROOT_URLCONF = 'clubbable.urls'
 
 WSGI_APPLICATION = 'clubbable.wsgi.application'
