5ae32b96e7072583d191876d253f3fd1c1ef901d
Norman Hooper <norman@kaapstorm.com>
2019-01-02 03:05:11 +0200

    import_image

--- src/clubbable/dropboxer/tasks.py
+++ src/clubbable/dropboxer/tasks.py
@@ -9,6 +9,7 @@ from dropbox.files import FileMetadata
 from club.models import User
 from clubbable.utils import quickcache
 from docs.models import Folder, Document
+from galleries.models import Gallery, Image
 from import_mdb.import_mdb import import_mdb
 
 
@@ -39,6 +40,14 @@ def get_folders():
     return {f.name.lower(): f for f in Folder.objects.all()}
 
 
+@quickcache([])
+def get_galleries():
+    """
+    Returns a dictionary of lower-case gallery names to gallery instances
+    """
+    return {g.name.lower(): g for g in Gallery.objects.all()}
+
+
 @quickcache(['folder_name'])
 def get_doc_file_ids(folder_name):
     """
@@ -53,11 +62,25 @@ def get_doc_file_ids(folder_name):
     }
 
 
-def get_folder(path):
+@quickcache(['gallery_name'])
+def get_img_file_ids(gallery_name):
+    """
+    Returns a set of the Dropbox file IDs of the images in a given
+    gallery
+    """
+    gallery = get_galleries()[gallery_name]
+    return {
+        i.dropbox_file_id
+        for i in gallery.image_set.all()
+        if i.dropbox_file_id
+    }
+
+
+def get_directory(path):
     """
     Returns the folder of a file specified by a path
 
-    >>> get_folder('/foo/bar/baz.txt')
+    >>> get_directory('/foo/bar/baz.txt')
     'bar'
 
     """
@@ -68,18 +91,28 @@ def get_folder(path):
 
 
 def is_new_document(entry):
-    folder_name = get_folder(entry.path_lower)
+    directory = get_directory(entry.path_lower)
     return (
             isinstance(entry, FileMetadata) and
-            folder_name and
-            folder_name in get_folders() and
-            entry.id not in get_doc_file_ids(folder_name)
+            directory and
+            directory in get_folders() and
+            entry.id not in get_doc_file_ids(directory)
+    )
+
+
+def is_new_image(entry):
+    directory = get_directory(entry.path_lower)
+    return (
+            isinstance(entry, FileMetadata) and
+            directory and
+            directory in get_galleries() and
+            entry.id not in get_img_file_ids(directory)
     )
 
 
 def import_document(dbx, entry):
-    folder_name = get_folder(entry.path_lower)
-    folder = get_folders()[folder_name]
+    directory = get_directory(entry.path_lower)
+    folder = get_folders()[directory]
     _, response = dbx.files_download(entry.path_lower)
     content_file = ContentFile(content=response.content, name=entry.name)
     document = Document(
@@ -89,6 +122,20 @@ def import_document(dbx, entry):
         file=UploadedFile(content_file),
     )
     document.save()
+
+
+def import_image(dbx, entry):
+    directory = get_directory(entry.path_lower)
+    gallery = get_galleries()[directory]
+    _, response = dbx.files_download(entry.path_lower)
+    content_file = ContentFile(content=response.content, name=entry.name)
+    image = Image(
+        gallery=gallery,
+        description=entry.name,
+        dropbox_file_id=entry.id,
+        original=UploadedFile(content_file),
+    )
+    image.save()
 
 
 @shared_task
@@ -121,6 +168,8 @@ def process_changes(username):
                         import_mdb(tmp_file.name)
                 elif is_new_document(entry):
                     import_document(dbx, entry)
+                elif is_new_image(entry):
+                    import_image(dbx, entry)
 
             cursor = result.cursor
             has_more = result.has_more
