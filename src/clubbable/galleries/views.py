fce6a27d29b9d281317488e0b997a7530de4fa59
Norman Hooper <norman@kaapstorm.com>
2019-01-02 02:19:47 +0200

    Add gallery to context

--- src/clubbable/galleries/views.py
+++ src/clubbable/galleries/views.py
@@ -8,6 +8,13 @@ from website.views import ClubbableContextMixin
 class ImageList(LoginRequiredMixin, ListView, ClubbableContextMixin):
     context_object_name = 'images'
 
+    def get_gallery(self):
+        return get_object_or_404(Gallery, pk=self.kwargs['gallery_id'])
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['gallery'] = self.get_gallery()
+        return context_data
+
     def get_queryset(self):
-        gallery = get_object_or_404(Gallery, pk=self.kwargs['gallery_id'])
-        return Image.objects.filter(gallery=gallery)
+        return self.get_gallery().image_set.all()
