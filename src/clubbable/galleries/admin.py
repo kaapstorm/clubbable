736b735b0109c082b7e940a96e22d317c8b24ca5
Norman Hooper <norman@kaapstorm.com>
2019-01-02 02:20:44 +0200

    Improve admin

--- src/clubbable/galleries/admin.py
+++ src/clubbable/galleries/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from galleries.models import Gallery, Image
 
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'gallery')
+    list_filter = ('gallery',)
+    search_fields = ('description',)
+
+
 admin.site.register(Gallery)
-admin.site.register(Image)
+admin.site.register(Image, ImageAdmin)
