9ac44300f7ce5c94d7500eb53f143fc09430eff3
Norman Hooper <norman@kaapstorm.com>
2019-06-15 16:17:27 +0200

    pytest and tests

---
+++ src/clubbable/dropboxer/tests.py
@@ -0,0 +1,24 @@
+from dropboxer.tasks import drop_ext
+
+
+def test_drop_ext():
+    filename = 'image.jpeg'
+    filename = drop_ext(filename, ('jpg', 'jpeg'))
+    assert filename == 'image'
+
+def test_drop_ext_upper_ext():
+    filename = 'IMAGE.JPG'
+    filename = drop_ext(filename, ('jpg', 'jpeg'))
+    assert filename == 'IMAGE'
+
+
+def test_drop_ext_no_ext():
+    filename = 'README'
+    filename = drop_ext(filename, ('jpg', 'jpeg'))
+    assert filename == 'README'
+
+
+def test_drop_ext_unknown_ext():
+    filename = 'hello.world'
+    filename = drop_ext(filename, ('jpg', 'jpeg'))
+    assert filename == 'hello.world'
